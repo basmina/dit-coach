467e5c0888cc59ef96e271322742f1cfee7ba161
basmina <145188743+basmina@users.noreply.github.com>
2026-06-04 13:55:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -39,7 +39,9 @@ async function connectDB() {
 
 // email transporter
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.GMAIL_ID,
     pass: process.env.GMAIL_PASSWORD
