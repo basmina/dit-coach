947b61f3f9c181447e3519d41b30dee4e79df2e0
basmina <145188743+basmina@users.noreply.github.com>
2026-06-04 11:57:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ async function connectDB() {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: "https://dit-coach.onrender.com/auth/google/callback"
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     // find or create user in MongoDB
